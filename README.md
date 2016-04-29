472108f0f73583db0cca8ce84146b30ae0b579a5
mxmlnkn <mxmlnkn@github.io>
2016-04-29 15:44:38 +0200

    rehaul

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # include-spider
+
+## Usage
+
+```
+incvis <source-file> [<inc-path>[:<inc-path[...]]]
+```
+E.g.
+```
+incvis src/libPMacc/include/nvidia/reduce/Reduce.hpp ./src/libPMacc/include
+```
+The output would be
+```
+src/libPMacc/include/nvidia/reduce/Reduce.hpp
+    ./src/libPMacc/include/nvidia/functors/Assign.hpp
+    ./src/libPMacc/include/traits/GetValueType.hpp
+        ./src/libPMacc/include/traits/GetValueType.tpp
+    ./src/libPMacc/include/pmacc_types.hpp
+        ./src/libPMacc/include/debug/PMaccVerbose.hpp
+            ./src/libPMacc/include/debug/VerboseLog.hpp
+                ./src/libPMacc/include/debug/VerboseLogMakros.hpp
+                    ./src/libPMacc/include/debug/VerboseLog.hpp -> already loaded
+                ./src/libPMacc/include/pmacc_types.hpp -> already loaded
+```
--- include-spider.bash
+++ include-spider.bash
@@ -1,99 +1,79 @@
+#!/bin/bash
+
 # ##############################################################################
 # Treat command line args
 # ##############################################################################
 if [ -z "$1" ]; then
-  echo "I need a root file to start from."
-  return 1
-else
-  ROOTFILE=$1
-  echo "Use root file: $ROOTFILE"
-  fi
-
-if [ -n "$2" ]; then
-  echo "Got include path specification: $2"
-  INC="$2"
-else
-  INC="./:./src/:./src/tests/"
-  echo "Got no include path specification, use: $INC"
+    echo "\e[31mPlease specify a source-file from which to start as the first argument"'!'"\e[0m"
+    exit 1
 fi
+rootFile=$1
+
+if [ ! -z "$2" ]; then INC=":$2"; fi
+INC=${rootFile%/*}$INC:.
 
 #INC_ARR=$(echo $INC | tr ":" "\n")
-IFS=":" read -a INC_ARR <<< $INC
-
-
-# ##############################################################################
-# Define some functions
-# ##############################################################################
-function is_not_in {
-  eval list=\${$1[@]}
-  for i in $list; do
-    if [ $i = $2 ]; then
-      return 1;
-      fi
-    done
-  return 0
-}
-
-function not_empty {
-  IFS=":" read -a a_ts < $1
-  return $(test ${#a_ts[@]} -gt 0)
-}
-
-function find_path {
-  fn=$1
-  for pre in ${INC_ARR[@]}; do
-    path="$pre/$fn"
-    if [ -f $path ]; then
-      echo $path
-      return 0
-      fi
-    done
-  return 1
-}
-
-function search_incs {
-  fn=$1
-  path=$(find_path $fn)
-  if [ $? -eq 0 ]; then
-    cat $path | grep "#include" | grep -v "<" | grep -v ^// | sed -e 's/#include "//;s/"//g'
-  else
-    echo ""
-    fi
-}
-
-
-# ##############################################################################
-# main
-# ##############################################################################
-dir="/tmp/include-spider/"
-if [ -d $dir ]; then :
-else
-  mkdir $dir
-  fi
-
-fn_ts="/tmp/include-spider/tmp_to_search.txt"
-fn_s="/tmp/include-spider/tmp_searched.txt"
-echo -ne "$ROOTFILE:" > $fn_ts
-echo -ne "" > $fn_s
-
-while $(not_empty $fn_ts); do
-  IFS=":" read -a a_ts < $fn_ts
-  IFS=":" read -a a_s  < $fn_s
-  next=${a_ts[0]}
-  echo -ne "" > $fn_ts; for i in ${a_ts[@]:1}; do echo -ne "$i:" >> $fn_ts; done
-#  echo "current target: $next"
-#  echo "still listed: $(cat $fn_ts)"
-  if $(is_not_in a_s $next); then
-#    echo "search result: $(search_incs $next)"
-    for i in $(search_incs $next); do
-      echo -ne "$i:" >> $fn_ts
-      done
-    echo -ne "$next:" >> $fn_s
-    fi
-#  echo ""
+IFS=":" read -a includePaths <<< "$INC"
+echo -e "\e[37mInclude Paths (first will be searched first):"
+for path in ${includePaths[@]}; do
+    echo "    $path"
 done
-echo -ne "\n" >> $fn_s
+echo -e "\e[0m"
 
-result=$(cat $fn_s | tr ":" "\n")
-echo -e "\n$ROOTFILE directly or indirectly depends on:\n\n${result[@]}"
+searchedPaths=()
 
+# printAllIncludes <source-file> <level>
+printAllIncludes() {
+    local file level indent includes i path foundPath
+    file=$1
+    level=$2
+
+    # print current file
+    indent=$(($level * 4))
+    printf "%.*s" $indent "                             "
+    #if [ ! $indent -eq 0 ]; then
+    #    printf "+ ";
+    #fi
+    printf "%s" "$file"
+
+    # search for file in paths to determine if already found and we can stop parsing it
+    local found=0
+    for path in ${searchedPaths[@]}; do
+        if [ "$path" == "$file" ]; then
+            printf " -> already loaded"
+            return 1
+        fi
+    done
+
+    # http://stackoverflow.com/questions/19771965/split-bash-string-by-newline-characters
+    IFS=$'\n' read -rd '' -a includes <<<"$(sed -nE 's/^[^/]*#[ \t]*include[ \t]*"([^"]*)".*/\1/p;' "$file")"
+    searchedPaths+=( "$file" )
+
+    # parse dependencies recursively
+    for (( i=0; i<${#includes[@]}; i++ )); do
+    {
+        # try to find file in include paths
+        foundPath=
+        for path in ${file%/*} ${includePaths[@]}; do
+            #echo -e "\e[36m path = $path \e[0m"
+            if [ -f "$path/${includes[i]}" ]; then
+                foundPath="$path/${includes[i]}"
+                break
+            fi
+        done
+        if [ ! -f "$foundPath" ]; then
+            echo -e "\e[31mCouldn't find '${includes[i]}' in any of the given include paths. Skipping this.\e[0m"
+            continue
+        fi
+
+        # not already parsed, so do it
+        if [ $found -eq 0 ]; then
+            printf "\n"
+            printAllIncludes "$foundPath" $(($2+1))
+        fi
+    }
+    done
+}
+
+printAllIncludes "$rootFile" 0
+printf "\n"
